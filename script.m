cd3a3488fc1bd99c0bd85699fa3b3f8e070a02cf
paolaallegrini <paola.allegrini@laposte.net>
2019-02-09 23:29:12 +0100

    fig tetra

--- script.m
+++ script.m
@@ -1,23 +1,23 @@
 function script
 close all
-fileID = fopen('cube.txt');
-str = fread(fileID,[1,inf],'char');
-str = char(str);
-eval(str);
+% fileID = fopen('cube.txt');
+% str = fread(fileID,[1,inf],'char');
+% str = char(str);
+% eval(str);
 
 % tetraedre
-% N=4;
-% D=3;
-% P = zeros(N,D); %terminaux
-% P = [0 0 0; 1 0 0; 0.5 0.86602540378 0.0; 0.5 0.28867513459 0.81649658092];
+N=4;
+D=3;
+P = zeros(N,D); %terminaux
+P = [0 0 0; 1 0 0; 0.5 0.86602540378 0.0; 0.5 0.28867513459 0.81649658092];
 
 
 [A,b,Aeq,beq]=contraintesyz(N,D);
 %Benders
 %Initialisation
 X = rand(N-2,D); %points steiner
-[y,z]=lienscube(N); %topo init cube
-%[y,z]=lienstetra(N); %topo init tetra
+%[y,z]=lienscube(N); %topo init cube
+[y,z]=lienstetra(N); %topo init tetra
 
 
 
